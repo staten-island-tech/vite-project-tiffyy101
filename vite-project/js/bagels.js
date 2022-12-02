ca42f9b9b2e1b1d0356e605e3e9866122acc48ce
tiffyy101 <bobatiffy101@gmail.com>
2022-12-02 10:16:37 -0500

    4th commit

---
+++ vite-project/js/bagels.js
@@ -0,0 +1,8 @@
+const bagels = [
+    {
+        name: "Everything Bagel",
+        info: "Savory bagels topped with a mixture of everything bagel seasoning, including garlic powder, onion powder, poppy seeds, sesame seeds, and sea salt.",
+        price: 6,
+        img: "image.png"
+    }
+]
