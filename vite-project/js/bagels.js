1aada2b74ae5398ef92ff0bb63761010d5903c3b
tiffyy101 <bobatiffy101@gmail.com>
2022-12-05 10:15:41 -0500

    5th commit

--- vite-project/js/bagels.js
+++ vite-project/js/bagels.js
@@ -2,7 +2,19 @@ const bagels = [
     {
         name: "Everything Bagel",
         info: "Savory bagels topped with a mixture of everything bagel seasoning, including garlic powder, onion powder, poppy seeds, sesame seeds, and sea salt.",
-        price: 6,
-        img: "image.png"
+        price: 5,
+        img: "https://thebusybaker.ca/wp-content/uploads/2020/05/homemade-everything-bagels-fb-ig-7-scaled.jpg"
+    },
+    {
+        name: "Jalapeno Cheddar Bagel",
+        info: "Includes cheese baked inside as well on top of the bagel, paired along with spicy jalapenos",
+        price: 7,
+        img: "https://thumbs.dreamstime.com/b/cheddar-jalapeno-bagel-delicious-cheese-rustic-wood-table-top-170935927.jpg"
+    }
+    {
+        name: "Smoked Salmon Bagel",
+        info: "",
+        price: 16,
+        img: "https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/smoked-salmon-bagel-su.jpg"
     }
 ]
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,4 @@
 import "../styles/style.css";
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
 <div>
