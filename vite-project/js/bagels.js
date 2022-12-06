d312cf67e5e38f28f4796717aa3e2ab7384339de
tiffyy101 <bobatiffy101@gmail.com>
2022-12-06 10:16:39 -0500

    6th commit

--- vite-project/js/bagels.js
+++ vite-project/js/bagels.js
@@ -1,20 +1,36 @@
 const bagels = [
     {
+        name: "Cinnamon Raisan Bagel",
+        info: "A sweet bagel with the crispiness of raisins and the subtle flavor of cinnamon",
+        price: 4,
+        img: "https://cdn.shopify.com/s/files/1/0549/1117/2815/products/bagelista_cinnamonraisin_2_900x.jpg?v=1654008008"
+    },
+    {
         name: "Everything Bagel",
-        info: "Savory bagels topped with a mixture of everything bagel seasoning, including garlic powder, onion powder, poppy seeds, sesame seeds, and sea salt.",
-        price: 5,
+        info: "Savory bagels topped with a mixture of everything bagel seasoning, including garlic powder, onion powder, poppy seeds, sesame seeds, and sea salt",
+        price: 3,
         img: "https://thebusybaker.ca/wp-content/uploads/2020/05/homemade-everything-bagels-fb-ig-7-scaled.jpg"
     },
     {
         name: "Jalapeno Cheddar Bagel",
         info: "Includes cheese baked inside as well on top of the bagel, paired along with spicy jalapenos",
-        price: 7,
+        price: 6,
         img: "https://thumbs.dreamstime.com/b/cheddar-jalapeno-bagel-delicious-cheese-rustic-wood-table-top-170935927.jpg"
-    }
+    },
     {
         name: "Smoked Salmon Bagel",
-        info: "",
-        price: 16,
+        info: "Homemade herbed cream cheese against smoky, buttery salmon, herbaceous dill against briny capers, in a fluffy, perfectly toasted bagel",
+        price: 15,
         img: "https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/smoked-salmon-bagel-su.jpg"
+    },
+    {
+        name: "Bacon Egg & Cheese Sandwhich",
+        info: "Crispy bacon pair with the fluffiest fried egg and slices of delicious gooey American cheese",  
+        price: 8,
+        img: "https://www.oliviascuisine.com/wp-content/uploads/2016/07/bacon-egg-and-cheese-1-735x1103.jpg"  
+    },
+    {
+        
     }
+
 ]
