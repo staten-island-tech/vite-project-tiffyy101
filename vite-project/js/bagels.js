473df7e0a44bdda09281bec463d3a173d02991c6
tiffyy101 <bobatiffy101@gmail.com>
2022-12-07 10:14:47 -0500

    7th commit

--- vite-project/js/bagels.js
+++ vite-project/js/bagels.js
@@ -1,8 +1,20 @@
 const bagels = [
     {
+        name: "OG Bagel",
+        info: "A simple yet delicious bagel, best paired with our homade cream cheese and jam",
+        price: 2,
+        img: "https://www.einsteinbros.com/wp-content/uploads/2019/04/EBB-Bagel-Plain-800x700-1.png"
+    },
+    {
+        name: "Sesame Bagel",
+        info: "A twist from our OG bagel, generously topped with roasted sesame seeds",
+        price: 3,
+        img: "https://www.einsteinbros.com/wp-content/uploads/2019/04/EBB-Bagel-SesameSeed-800x700-1.png"
+    },
+    {
         name: "Cinnamon Raisan Bagel",
         info: "A sweet bagel with the crispiness of raisins and the subtle flavor of cinnamon",
-        price: 4,
+        price: 3,
         img: "https://cdn.shopify.com/s/files/1/0549/1117/2815/products/bagelista_cinnamonraisin_2_900x.jpg?v=1654008008"
     },
     {
@@ -14,23 +26,31 @@ const bagels = [
     {
         name: "Jalapeno Cheddar Bagel",
         info: "Includes cheese baked inside as well on top of the bagel, paired along with spicy jalapenos",
-        price: 6,
+        price: 5,
         img: "https://thumbs.dreamstime.com/b/cheddar-jalapeno-bagel-delicious-cheese-rustic-wood-table-top-170935927.jpg"
     },
     {
         name: "Smoked Salmon Bagel",
         info: "Homemade herbed cream cheese against smoky, buttery salmon, herbaceous dill against briny capers, in a fluffy, perfectly toasted bagel",
-        price: 15,
+        price: 12,
         img: "https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/smoked-salmon-bagel-su.jpg"
     },
     {
         name: "Bacon Egg & Cheese Sandwhich",
         info: "Crispy bacon pair with the fluffiest fried egg and slices of delicious gooey American cheese",  
-        price: 8,
+        price: 6,
         img: "https://www.oliviascuisine.com/wp-content/uploads/2016/07/bacon-egg-and-cheese-1-735x1103.jpg"  
     },
     {
-        
+       name: "Crspy Chicken Bagel", 
+       info: "Perfectly tender chicken with a crisy crunch on the outside, paired with our delicious homemade sauce",
+       price: 8,
+       img: "https://dempsters.ca/sites/default/files/styles/recipes_full_467x341_/public/2021-02/SundriedTomatoBagel-ChickenSandwich_Sitev2.jpg?itok=MAAegaRq"
+    },
+    {
+        name: "Caprese Bagel",
+        info: "",
+        price: 12,
+        img: "https://www.lastingredient.com/wp-content/uploads/2019/07/caprese-bagel3.jpg"
     }
-
 ]
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,5 +2,5 @@ import "../styles/style.css";
 
 document.querySelector('#app').innerHTML = `
 <div>
-<h1>Best Bagel Shop</h1>
+<h1>Best Bagel Shop in the Entire Universe and Beyond</h1>
 </div>`
