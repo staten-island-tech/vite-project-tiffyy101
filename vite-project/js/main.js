d928a1eb4d74eb830bbe893800108cb6a790b113
tiffyy101 <bobatiffy101@gmail.com>
2022-12-17 15:32:01 -0500

    11th commit

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,41 @@
-import "../styles/style.css";
-import { menu } from "./menu";
+import "/styles/style.css";
+import {menu} from "./menu";
+console.log("menu");
 
-document.getElementById('#app').innerHTML = `
-<div>
-<h1>Best Bagel Shop</h1>
-</div>`
-;
+const DOMSelectors = {
+    button: document.getElementById("btn"),
+    h1: document.getElementById("shop"),
+    mainDiv: document.getElementById("main")
 
-document.querySelector(".btn").addEventListener("click", function() {
-    console.log("menu");
-})
+};
+
+const createMenu = function (menu) {
+    document.getElementById("main").insertAdjacentHTML
+        ("beforeend",
+            `
+          <div id="mainCard">
+          <p><img src=${menu.img}></p>
+          <h2>${menu.name}</h2>
+    
+          <p id="outputText">Type: ${menu.price}</p>
+          <p id="outputText">Price: ${menu.info}</p>
+          <button id="buy">Choose</button>
+          </div>
+        `
+        );
+}
+
+menu
+.forEach((item) => createMenu(car));
+
+menu
+.filter((food) => food.bestSeller === "yes")
+.forEach(food => {
+    console.log(food.name);
+});
+
+menu
+.filter((food) => food.drink === "yes")
+.forEach(food => {
+    console.log(food.name);
+});
